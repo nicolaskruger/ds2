fb67f47c96e858383ea80f965a1922168d42d1cd
nicolas-kruger <nicolas.nkruger@gmail.com>
2021-01-06 16:03:48 -0300

    PUT /orders/{id}/delivered

--- backend/src/main/java/com/devsuperior/dsdeliver/controllers/OrderController.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/controllers/OrderController.java
@@ -30,4 +30,16 @@ public class OrderController {
                 .buildAndExpand(dto.getId()).toUri();
         return ResponseEntity.created(uri).body(dto);
     }
+//    @PutMapping
+//    public ResponseEntity<OrderDTO> update(@RequestBody OrderDTO dto){
+//        dto = service.update(dto);
+//        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+//                .buildAndExpand(dto.getId()).toUri();
+//        return ResponseEntity.created(uri).body(dto);
+//    }
+    @PutMapping("/{id}/delivered")
+    public ResponseEntity<OrderDTO> setDelivered(@PathVariable Long id){
+        OrderDTO dto = service.setDelivered(id);
+        return  ResponseEntity.ok().body(dto);
+    }
 }
--- backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -39,5 +39,23 @@ public class OrderService {
         order = repository.save(order);
         return new OrderDTO(order);
     }
+    @Transactional
+    public OrderDTO update(OrderDTO dto){
+        Order order = new Order(dto.getId(),dto.getAddress(),dto.getLatitude(),dto.getLongitude(),dto.getMoment(),
+                dto.getStatus());
+        for(ProductDTO p : dto.getProducts()){
+            Product product = productRepository.getOne(p.getId());
+            order.getProducts().add(product);
+        }
+        order = repository.save(order);
+        return new OrderDTO(order);
+    }
+    @Transactional
+    public OrderDTO setDelivered(Long id){
+        Order order = repository.getOne(id);
+        order.setStatus(OrderStatus.DELIVERED);
+        order = repository.save(order);
+        return new OrderDTO(order);
+    }
 
 }
